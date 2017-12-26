deb82c86e2883d71033cb5f1929477dc9d9742a6
pezy <urbancpz@gmail.com>
2017-12-26 17:17:46 +0800

    improving version.

--- version/version_from_svn.py
+++ version/version_from_svn.py
@@ -1,8 +1,8 @@
 import subprocess, re
 result = subprocess.run(['svnversion'], stdout=subprocess.PIPE)
-version = result.stdout.decode('utf-8')[:4]
+version = result.stdout.decode('utf-8')
 
-subversion = '5,3,3,' + version[1];
+subversion = '0,0,0,' + version
 print(subversion)
 print(version)
 
@@ -11,13 +11,14 @@ file_regex = re.compile(r"\b(FILEVERSION|FileVersion) \d+,\d+,\d+,\d+\b", re.MUL
 prod_regex = re.compile(r"\b(PRODUCTVERSION|ProductVersion) \d+,\d+,\d+,\d+\b", re.MULTILINE)
 ret = re.sub(file_regex, r"\1 " + subversion, str(file.read()))
 ret = re.sub(prod_regex, r"\1 " + ",".join(version), ret)
+file.close()
 
-file.close();
 file = open("app.rc", "w")
 file.write(ret)
-file.close();
+file.close()
 
 file = open("svn.hpp", "w")
 file.write('#pragma once\n')
 file.write('#define VER_PRODUCTVERSION \"' + subversion + '\"\n')
 file.write('#define VER_SVN_REVISION ' + version)
+file.close()
